e37b8e9996364f30df43b7be6a09d1c3813d0d56
xXRadPlayzXx <74791468+xXRadPlayzXx@users.noreply.github.com>
2020-12-23 16:44:45 +0000

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -4,7 +4,7 @@ config();
 const client = new RadClient();
 client.start({
   commandsDir: "Commands",
-  token: "Nzc5NDE3Njc3NTc1ODgwNzg0.X7gPRw.JBKZiu5r9Qlwt1JKevGBtf5cV98",
+  token: "Nzc5NDE3Njc3NTc1ODgwNzg0.X7gPRw.-KYcy1i1TJ4vCMs4u762ijuzjZ4",
   defualtPrefix: "!",
   eventsDir: "Events",
 });
