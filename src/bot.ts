bf52016dd78f9d62298b77d2e512a14056116af8
RadiDzYt <radimebdzyt@gmail.com>
2020-12-23 18:10:12 +0000

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -4,7 +4,7 @@ config();
 const client = new RadClient();
 client.start({
   commandsDir: "Commands",
-  token: "Nzc5NDE3Njc3NTc1ODgwNzg0.X7gPRw.-KYcy1i1TJ4vCMs4u762ijuzjZ4",
+  token: "Nzc5NDE3Njc3NTc1ODgwNzg0.X7gPRw.kOdlg6LMjpjjT1nETiMYH3RGjuc",
   defualtPrefix: "!",
   eventsDir: "Events",
 });
