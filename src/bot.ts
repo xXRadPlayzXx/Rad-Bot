c6160e9a1b59e7843ee5dc83410e120deb8e9cda
RadiDzYt <radimebdzyt@gmail.com>
2020-12-23 18:11:29 +0000

    a

--- src/bot.ts
+++ src/bot.ts
@@ -4,7 +4,7 @@ config();
 const client = new RadClient();
 client.start({
   commandsDir: "Commands",
-  token: "Nzc5NDE3Njc3NTc1ODgwNzg0.X7gPRw.kOdlg6LMjpjjT1nETiMYH3RGjuc",
+  token: "Nzc5NDE3Njc3NTc1ODgwNzg0.X7gPRw.p_9AojeKZVgaYTF59pD6nao_EKo",
   defualtPrefix: "!",
   eventsDir: "Events",
 });
