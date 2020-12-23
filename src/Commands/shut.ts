f74f75918f9f01c51868f9e0e778056a9f7d06f5
xXRadPlayzXx <74791468+xXRadPlayzXx@users.noreply.github.com>
2020-12-23 14:28:51 +0000

    Update shut.ts

--- src/Commands/shut.ts
+++ src/Commands/shut.ts
@@ -1,9 +1,10 @@
 export = {
-  name: "shutdown",
-  ownerOnly: true,
-  category: 'Dev',
-  description: 'Shuts down the bot'
-  callback: (message, args, bot) => {
-    process.exit()
-  },
-};
+    name: "shutdown",
+    ownerOnly: true,
+    category: 'Dev',
+    description: 'Shuts down the bot',
+    callback: (message, args, bot) => {
+      process.exit()
+    },
+  };
+  
