cb221c4dc08b491fdeaebacc0b7c65923fa57fca
RadiDzYt <radimebdzyt@gmail.com>
2020-12-24 22:31:52 +0000

    a

--- start.cmd
+++
@@ -1,9 +0,0 @@
-set ghost = wscript.CreateObject("WScript.Shell")
-
-ghost.run "notepad"
-wscript.sleep 3000
-ghost.sendkeys "Hello."
-wscript.sleep 1000
-ghost.sendkeys "I am a magical ghost "
-wscript.sleep 1000
-ghost.sendkeys "who possesed your keyboard."
